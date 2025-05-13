f5d3e4f2a31100a9794d690d18bdf1f02ffd4e7d
Kevin <kvnnedd@gmail.com>
2025-05-13 00:11:24 -0400

    added some modal input functionality

--- scripts/index.js
+++ scripts/index.js
@@ -3,22 +3,56 @@ const editProfileModal = document.querySelector("#edit-profile-modal");
 const profileModalCloseBtn =
   editProfileModal.querySelector(".modal__close-btn");
 
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+
+const profileFormElement = document.querySelector(".modal__form");
+const profileNameInput = editProfileModal.querySelector("#profile-name");
+const profileDescriptionInput = editProfileModal.querySelector("#description");
+
 const newPostBtn = document.querySelector(".profile__new-post-btn");
 const newPostModal = document.querySelector("#new-post-modal");
-const PostModalCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const newPostModalCloseBtn = newPostModal.querySelector(".modal__close-btn");
+
+const addCardFormElement = newPostModal.querySelector(".modal__form");
+const newPostCaptionInput = newPostModal.querySelector("#caption");
+const newPostLinkInput = newPostModal.querySelector("#image-link");
 
 editProfileBtn.addEventListener("click", function () {
   editProfileModal.classList.add("modal_is-open");
+  profileNameInput.value = profileName.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
 });
 
 profileModalCloseBtn.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_is-open");
 });
 
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileName.textContent = profileNameInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-open");
+}
+
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
+
+function handleNewPostFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.log(newPostLinkInput.value);
+  console.log(newPostCaptionInput.value);
+
+  newPostModal.classList.remove("modal_is-open");
+}
+
+addCardFormElement.addEventListener("submit", handleNewPostFormSubmit);
+
 newPostBtn.addEventListener("click", function () {
   newPostModal.classList.add("modal_is-open");
 });
 
-PostModalCloseBtn.addEventListener("click", function () {
+newPostModalCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-open");
 });
